89dfcc4a3660c28228a250d81d44dca65aac42e8
ghazal-jamalzade <jmzd.developer94@gmail.com>
2022-11-28 11:04:24 +0330

    15 - refresh token data source & repository

--- lib/data/source/auth_data_source.dart
+++ lib/data/source/auth_data_source.dart
@@ -3,6 +3,7 @@ import 'package:flutter_online_shop/common/constants.dart';
 import 'package:flutter_online_shop/common/http_response_validator.dart';
 import 'package:flutter_online_shop/data/auth_info.dart';
 
+
 abstract class IAuthDataSource {
   Future<AuthInfo> login(String username, String password);
   Future<AuthInfo> signUp(String username, String password);
@@ -32,9 +33,18 @@ class AuthRemoteDataSource
   }
 
   @override
-  Future<AuthInfo> refreshToken(String token) {
-    // TODO: implement refreshToken
-    throw UnimplementedError();
+  Future<AuthInfo> refreshToken(String token) async {
+    final response = await httpClient.post("auth/token", data: {
+      "grant_type": "refresh_token",
+      "refresh_token": token,
+      "client_id": 2,
+      "client_secret": Constants.clientSecret
+    });
+
+    validateResponse(response);
+
+    return AuthInfo(
+        response.data["access_token"], response.data["refresh_token"]);
   }
 
   @override
