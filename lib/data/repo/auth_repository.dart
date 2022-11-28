6c9b15519227cee31c13a55c4075a31be0c492cb
ghazal-jamalzade <jmzd.developer94@gmail.com>
2022-11-28 10:45:11 +0330

    14 -  signup data source & repository

--- lib/data/repo/auth_repository.dart
+++ lib/data/repo/auth_repository.dart
@@ -3,10 +3,12 @@ import 'package:flutter_online_shop/common/http_client.dart';
 import 'package:flutter_online_shop/data/auth_info.dart';
 import 'package:flutter_online_shop/data/source/auth_data_source.dart';
 
+
 final authRepository = AuthRepository(AuthRemoteDataSource(httpClient));
 
 abstract class IAuthRepository {
   Future<void> login(String username, String password);
+  Future<void> signUp(String username, String password);
 }
 
 class AuthRepository implements IAuthRepository {
@@ -17,5 +19,15 @@ class AuthRepository implements IAuthRepository {
   Future<void> login(String username, String password) async {
     final AuthInfo authInfo = await dataSource.login(username, password);
     debugPrint("access token is: " + authInfo.accessToken);
+  }
+
+  @override
+  Future<void> signUp(String username, String password) async {
+    try {
+      final AuthInfo authInfo = await dataSource.signUp(username, password);
+      debugPrint("access token is: " + authInfo.accessToken);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
