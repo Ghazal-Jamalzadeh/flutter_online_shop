859cbcef74b5fd538f45ebb3a5d2b55aa5f41e3a
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-04 17:32:46 +0430

    add notes about equatable

--- lib/common/http_response_validator.dart
+++ lib/common/http_response_validator.dart
@@ -4,6 +4,7 @@ import 'package:flutter_online_shop/common/exceptions.dart';
 
 mixin HttpResponseValidator{
   validateResponse(Response response) {
+    print(response);
     if (response.statusCode != 200) {
       throw AppException();
     }
